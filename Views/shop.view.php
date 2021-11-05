d680e35ba8efc98a7f3d8db670ceead28f91a0ce
opheus2 <j.ominiabohs@gmail.com>
2021-11-05 09:09:22 +0800

    revamp from php template to alpine

--- Views/shop.view.php
+++ Views/shop.view.php
@@ -1,27 +1,19 @@
-<?php
-
-// echo '<pre>';
-// var_dump($_SESSION);
-// echo '</pre>';
-?>
 <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
     <h2 class="text-2xl font-extrabold tracking-tight text-gray-900">Products of the day!</h2>
 
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        <?php foreach ($products as $product) : ?>
+        <template x-for="(product, index) in Object.values($store.app.products)" :key="index">
             <div class="group relative">
                 <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                    <img src="/images/<?= $product['image'] ?>" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+                    <img :src="`/images/${product.image}`" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
                 </div>
                 <div class="mt-4 flex justify-between">
                     <div>
                         <h3 class="text-sm text-gray-700">
-                            <a href="#">
-                                <?= $product['name'] ?>
-                            </a>
+                            <a href="#" x-text="product.name"></a>
                         </h3>
                     </div>
-                    <p class="text-sm font-medium text-gray-900"><?= $product['currency']->symbol ?><?= $product['amount'] ?></p>
+                    <p class="text-sm font-medium text-gray-900" x-text="`${product.currency.symbol}${product.amount}`"></p>
                 </div>
                 <div class="mt-4 flex justify-between">
                     <div>
@@ -32,32 +24,34 @@
                             </svg>
                         </h3>
                     </div>
-                    <button @click="addProductToCart(<?= $product['id'] ?>)" type="button" class="flex justify-center items-center px-2 py-1 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                    <button @click="addProductToCart(product.id)" type="button" class="flex justify-center items-center px-2 py-1 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                         </svg>
                     </button>
                 </div>
                 <div class="mt-4 flex justify-between">
-                    <div x-data="{ temp: 0, orig: 0, allowRating: true }" class="flex cursor-pointer text-4xl" @click="() => {orig = temp; rateProduct(<?=$product['id'] ?>, temp);}">
+                    <div x-data="{ temp: product.average_rating, orig: product.average_rating, allowRating: true }" class="flex cursor-pointer text-4xl" @click="() => {if(!allowRating) {return false}; orig = temp; rateProduct(product.id, temp); allowRating = false;}">
                         <input type="number" :value="orig" class="hidden" />
                         <template x-for="item in [1,2,3,4,5]">
-                            <span @mouseenter="temp = item" @mouseleave="temp = orig" class="text-gray-300 text-base" :class="{'text-purple-600': (temp >= item)}">★</span>
+                            <span @mouseenter="() => {if(!allowRating) {return false}; temp = item;}" @mouseleave="() => {if(!allowRating) {return false}; temp = orig;}" class="text-gray-300 text-base" :class="{'text-purple-600': (temp >= item)}">★</span>
                         </template>
                     </div>
-                    <p class="text-sm text-gray-700">20 ratings</p>
+                    <template x-if="product.average_rating == 0">
+                        <p class="text-sm text-gray-700">leave a rating</p>
+                    </template>
+                    <template x-if="product.average_rating > 0">
+                        <p class="text-sm text-gray-700" x-text="`${product.average_rating} avg rating`"></p>
+                    </template>
                 </div>
             </div>
-        <?php endforeach ?>
-
-        <!-- More products... -->
+        </template>
     </div>
 </div>
 <script>
     var datas = <?php echo json_encode($products) ?>;
     document.addEventListener("alpine:init", () => {
         Alpine.store('app').products = datas
-        console.log(Alpine.store('app').products)
     });
 </script>
 <?php include 'includes/shop/cart.view.php' ?>
