cd5c9d5b548cff3de8e1221ea2b324cd6a6cdab1
opheus2 <j.ominiabohs@gmail.com>
2021-11-06 20:45:20 +0800

    add htaccess for webserver and logout

--- Views/layouts/main.view.php
+++ Views/layouts/main.view.php
@@ -134,6 +134,14 @@
                                     <span class="sr-only">items in cart, view bag</span>
                                 </a>
                             </div>
+                            <form action="/logout" method="POST" class="ml-4 flow-root lg:ml-6 inline-flex">
+                                <button type="submit" class="group -m-2 p-2 flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                                    </svg>
+                                    <span class="sr-only">items in cart, view bag</span>
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
